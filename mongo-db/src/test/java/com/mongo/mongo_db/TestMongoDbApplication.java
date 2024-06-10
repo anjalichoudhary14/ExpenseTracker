e2134f88280f40cfbb19033fba2571d230c327d6
Anjali Choudhary <66716436+anjalichoudhary14@users.noreply.github.com>
2024-06-10 11:48:39 +0530

    Initial boilerplate

---
+++ mongo-db/src/test/java/com/mongo/mongo_db/TestMongoDbApplication.java
@@ -0,0 +1,11 @@
+package com.mongo.mongo_db;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(MongoDbApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
